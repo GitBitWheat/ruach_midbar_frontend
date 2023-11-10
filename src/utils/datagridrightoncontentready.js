2d85ea54a052f676418e8c6390d56d261485c192
GitBitWheat <ilay.daniel.04@gmail.com>
2023-11-11 01:24:59 +0200

    Fixed scroll to right in placements page

--- src/utils/datagridrightoncontentready.js
+++ src/utils/datagridrightoncontentready.js
@@ -1,5 +1,8 @@
 /** @param {import('devextreme/ui/data_grid').ContentReadyEvent} event */
 export const dataGridRightOnContentReady = event => {
-    const maxScrollLeft = event.element.scrollWidth - event.element.clientWidth;
-    event.component.option('scrollLeft', maxScrollLeft);
+    if (event.component.getScrollable()) {
+        const maxScrollLeft =
+            event.component.getScrollable().scrollWidth() - event.component.getScrollable().clientWidth();
+        event.component.getScrollable().scrollBy({ left: maxScrollLeft, top: 0 });
+    }
 };
