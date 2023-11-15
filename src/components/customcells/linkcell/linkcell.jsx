4b082ed9d4f8879c54357db5c237a8b88307d4cd
GitBitWheat <ilay.daniel.04@gmail.com>
2023-11-15 19:59:32 +0200

    Fixed LinkCell JSDoc

--- src/components/customcells/linkcell/linkcell.jsx
+++ src/components/customcells/linkcell/linkcell.jsx
@@ -2,15 +2,7 @@
  * Custom cell of DataGrid. Displays a link.
  * @param {Object} props The properties of the component.
  *      - value (Object): The value object of the cell.
- * @param {Object} props.value The value object of the cell.
- *      - 'link' (string): Link string of the cell: '\<text\>#\<url\>'
- *      - 'text' (string): Text of the link. Used if link isn't used.
- *      - 'url' (string): URL of the link. Used if link isn't used.
- *      - 'newTab' (boolean): Whether the link opens in a new tab or not
- * @param {string} props.value.link
- * @param {string} props.value.text
- * @param {string} props.value.url
- * @param {boolean} props.value.newTab
+ * @param {String} props.value
  */
 const LinkCell = ({ value }) => {
 
@@ -33,7 +25,5 @@ const LinkCell = ({ value }) => {
         </a>
     );
 };
-
-
 
 export default LinkCell;
